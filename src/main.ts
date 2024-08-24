39c196a9c30f6c8be91aecf9e3fef243e5c11744
Allan araya <alarayaro@est.utn.ac.cr>
2024-08-23 21:29:04 -0600

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,20 @@ import { join } from 'path';
 async function bootstrap() {
   dotenv.config({ path: './.env' });
   const app = await NestFactory.create(AppModule);
+
+  const grpcPort = process.env.GRPC_PORT || '50058'; // Usa el puerto de la variable de entorno o un predeterminado
+  const grpcUrl = `localhost:${grpcPort}`;
+
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
       package: 'auth',
       protoPath: join(__dirname, 'protos/users.proto'),
-      url: 'localhost:50058', // Puerto del servicio gRPC
+      url: grpcUrl, // Puerto del servicio gRPC
     },
   });
+
   await app.startAllMicroservices();
-  await app.listen(3007);
 }
+
 bootstrap();
